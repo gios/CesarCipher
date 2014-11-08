c3219342b75af9a817f02567e8ed4ff5899de342
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2014-11-08 17:45:30 +0200

    Fixed alphabet

--- src/cesarCode.js
+++ src/cesarCode.js
@@ -1,27 +1,29 @@
 function cesarDecode(data) {
-    var alphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя0123456789.,;:!?- ",
+    var alphabet = "АБВГДЕЄЖЗИІЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиійклмнопрстуфхцчшщьюя0123456789.,;:!?- ",
         decoder = '',
         key = 3,
         alphabetLength = alphabet.length,
         dataArray = [];
     
+    // Data to Array
     for(var number in data) {
         dataArray.push(data[number]);
     }
-    
-    console.log(dataArray + " " + alphabetLength);
+    // END Data to Array
 
     for (var i = 0; i <= dataArray.length - 1; i++) {
         for (var j = 0; j <= alphabetLength - 1; j++) {
             if (dataArray[i] === alphabet[j]) {
+                
                 var decoderFormule = (j - key) % alphabetLength;
                 console.log(decoderFormule);
 
                 while (decoderFormule < 0)
-                    decoderFormule += alphabetLength;
+                    decoderFormule += alphabetLength + Math.abs(decoderFormule);
 
                 while (decoderFormule >= alphabetLength)
                     decoderFormule -= alphabetLength;
+                
                 decoder = decoder + alphabet[decoderFormule];
                 console.log(decoder);
             }
